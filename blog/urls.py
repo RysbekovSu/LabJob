a32f500b537d4a03b52a139c548b8a43a67f7295
RysbekovSu <rsbkvsltnbk@gmail.com>
2022-12-08 14:28:43 +0600

    Finale

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+app_name = 'users'
 urlpatterns = [
+    path('registration/', views.Registration.as_view(), name='registration'),
     path('post/', views.blog_all, name='post'),
     path('post_detail/<int:id>', views.PostDetail.as_view(), name='detail'),
     path('add-post/', views.PostCreate.as_view(), name='add-post'),
+    path('login/', views.NewLoginForm.as_view(), name='login'),
+    path('users/', views.UserListView.as_view(), name='user_list'),
+    path('post/<int:id>/update/', views.PostUpdate.as_view(), name='information'),
+    path('post/<int:id>/delete/', views.DeletePost.as_view(), name='information'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,37 @@ from . import models,forms
 from django.http import HttpResponse
 from django.views import generic
 from .models import Post
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse
+from django.views.generic import CreateView, ListView
+
+class Registration(CreateView):
+    form_class = UserCreationForm
+    success_url = '/post/'
+    template_name = 'registration.html'
+
+
+
+class NewLoginForm(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+
+    def get_success_url(self):
+        return reverse("users:user_list")
+
+class UserListView(ListView):
+    queryset = User.objects.all()
+    template_name = 'user_list.html'
+
+    def get_queryset(self):
+        return User.objects.all()
+
+
+
+
 # Create your views here.
 def blog_all(request):
     post = models.Post.objects.all()
@@ -10,7 +41,6 @@ def blog_all(request):
 
 class PostDetail(generic.DetailView):
     template_name = 'PostDetail.html'
-    
     def get_object(self, **kwargs):
         post_id = self.kwargs.get('id')
         return get_object_or_404(models.Post, id=post_id)
@@ -24,3 +54,22 @@ class PostCreate(generic.CreateView):
         print(form.cleaned_data)
         return super(PostCreate, self).form_valid(form=form)
 
+class DeletePost(generic.DeleteView):
+    template_name = 'delete_post.html'
+    success_url = '/post/'
+
+    def get_object(self, **kwargs):
+        home_id = self.kwargs.get('id')
+        return get_object_or_404(models.Post, id=home_id)
+
+class PostUpdate(generic.UpdateView):
+    template_name = 'update_post.html'
+    form_class = forms.PostForm
+    success_url = '/post/'
+
+    def get_object(self, **kwargs):
+        home_id = self.kwargs.get('id')
+        return get_object_or_404(models.Post, id=home_id)
+
+    def form_valid(self, form):
+        return super(PostUpdate, self).form_valid(form=form)
